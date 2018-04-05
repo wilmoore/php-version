bb598af9fa4da55efb0395eed2be79d89697bc99
Jon Phipps <jphipps@madcreek.com>
2018-04-05 16:23:58 -0400

    revert the use of builtin commands
    It’s complicated, but I was updating the last release installed by homebrew rather than the latest master, which doesn’t actually work without removing these builtin commands.

--- php-version.sh
+++ php-version.sh
@@ -17,10 +17,6 @@ function php-version {
   # PHP installation paths
   local _PHP_VERSIONS=""
 
-  # use builtin commands for sort and grep
-  local grep="command grep -E"
-  local sort="command sort -r -t . -k 1,1n -k 2,2n -k 3,3n"
-
   # add ~/.phps if it exists (default)
   if [[ -d $HOME/.phps ]]; then
     export _PHP_VERSIONS="$_PHP_VERSIONS $HOME/.phps"
@@ -28,7 +24,7 @@ function php-version {
 
   # add default Homebrew directories if brew is installed
   if [[ -n $(command -v brew) ]]; then
-    export _PHP_VERSIONS="$_PHP_VERSIONS $(echo $(find $(brew --cellar) -maxdepth 1 -type l | $grep 'php[0-9]*$'))"
+    export _PHP_VERSIONS="$_PHP_VERSIONS $(echo $(find $(brew --cellar) -maxdepth 1 -type l | grep -E 'php[0-9]*$'))"
   fi
 
   # add extra directories if configured
@@ -108,7 +104,7 @@ function php-version {
         done
       done
 
-      _PHP_VERSIONS=$(IFS=$'\n'; echo "${_PHP_VERSIONS[*]}" | eval $sort)
+      _PHP_VERSIONS=$(IFS=$'\n'; echo "${_PHP_VERSIONS[*]}" | sort -r -t . -k 1,1n -k 2,2n -k 3,3n)
 
       for version in $(echo $_PHP_VERSIONS | tr " " "\n"); do
         local selected=" "
@@ -145,7 +141,7 @@ function php-version {
       done
     done
 
-    _TARGET_VERSION_FUZZY=$(IFS=$'\n'; echo "${_TARGET_VERSION_FUZZY[*]}" | eval $sort | eval "$grep '^$_TARGET_VERSION' 2>/dev/null" | tail -1)
+    _TARGET_VERSION_FUZZY=$(IFS=$'\n'; echo "${_TARGET_VERSION_FUZZY[*]}" | sort -r -t . -k 1,1n -k 2,2n -k 3,3n | grep -E "^$_TARGET_VERSION" 2>/dev/null | tail -1)
 
     for _PHP_REPOSITORY in "${_PHP_REPOSITORIES[@]}"; do
       for _dir in $(find -H $_PHP_REPOSITORY -maxdepth 1 -mindepth 1 -type d 2>/dev/null); do
